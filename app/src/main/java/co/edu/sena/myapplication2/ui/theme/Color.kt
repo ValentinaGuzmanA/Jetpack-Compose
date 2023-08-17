81798ea48c34685010885653703d71b227b0fcca
daniielaCR <naniscr0416@gmail.com>
2023-08-17 07:57:13 -0500

    comentario

--- app/src/main/java/co/edu/sena/myapplication2/ui/theme/Color.kt
+++ app/src/main/java/co/edu/sena/myapplication2/ui/theme/Color.kt
@@ -3,6 +3,6 @@ package co.edu.sena.myapplication2.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Navy = Color(0xFF073042)
-val Blue = Color(0xFF14B9CE)
+val Blue = Color(0xFF1417CE)
 val LightBlue = Color(0xFF0F41F7)
 val Chartreuse = Color(0x171816)
